3d2d8702ac5e472199ec1206162475e520b368cd
BeaconCat <2262965540@qq.com>
2026-08-11 20:11:04 +0800

    rk3576: sdmmc: enable clocks for the SDIO slot
    Signed-off-by: BeaconCat <2262965540@qq.com>

--- chips/rk3576/rk3576_sdmmc.c
+++ chips/rk3576/rk3576_sdmmc.c
@@ -1814,6 +1814,7 @@ static int rk3576_sdmmc_enable_sdio_clock(struct rk3576_sdmmc_dev_s *priv)
 struct sdio_dev_s *rk3576_sdmmc_initialize(int slotno)
 {
   struct rk3576_sdmmc_dev_s *priv;
+  int ret;
 
   if (slotno < 0 || slotno >= RK3576_SDMMC_NHOSTS)
     {
@@ -1833,6 +1834,20 @@ struct sdio_dev_s *rk3576_sdmmc_initialize(int slotno)
   priv->irq = g_sdmmc_config[slotno].irq;
   priv->clkin = g_sdmmc_config[slotno].clkin;
   priv->nonremovable = g_sdmmc_config[slotno].nonremovable;
+
+#ifdef CONFIG_RK3576_SDIO
+  if (slotno == RK3576_SDIO_SLOT)
+    {
+      ret = rk3576_sdmmc_enable_sdio_clock(priv);
+      if (ret < 0)
+        {
+          mcerr("ERROR: failed to enable SDIO clock: %d\n", ret);
+          return NULL;
+        }
+    }
+#else
+  UNUSED(ret);
+#endif
 
 #ifdef CONFIG_SDIO_DMA
   priv->descs = g_idmac_descs[slotno];
