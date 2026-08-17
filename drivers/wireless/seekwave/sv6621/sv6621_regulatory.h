b5dd6622473fe67ef7038071c3407f4e50a45fd5
BeaconCat <2262965540@qq.com>
2026-08-17 23:50:25 +0800

    wifi: sv6621: enforce radio policy
    Signed-off-by: BeaconCat <2262965540@qq.com>

--- drivers/wireless/seekwave/sv6621/sv6621_regulatory.h
+++ drivers/wireless/seekwave/sv6621/sv6621_regulatory.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * drivers/wireless/seekwave/sv6621/sv6621_core.h
+ * drivers/wireless/seekwave/sv6621/sv6621_regulatory.h
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -20,53 +20,35 @@
  *
  ****************************************************************************/
 
-#ifndef __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_CORE_H
-#define __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_CORE_H
+#ifndef __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_REGULATORY_H
+#define __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_REGULATORY_H
 
 /****************************************************************************
  * Included Files
  ****************************************************************************/
 
 #include <nuttx/config.h>
-#include <nuttx/mutex.h>
 
 #include "include/sv6621.h"
 #include "sv6621_command.h"
-#include "sv6621_packet.h"
-#include "sv6621_regulatory.h"
-#include "sv6621_rx.h"
 #include "sv6621_scan.h"
-#include "sv6621_service.h"
-#include "sv6621_tx.h"
-#include "sv6621_wifi.h"
 
 /****************************************************************************
- * Public Types
+ * Pre-processor Definitions
  ****************************************************************************/
 
-struct sv6621_dev_s
-{
-  struct sv6621_config_s config;
-  mutex_t lifecycle_lock;
-  mutex_t status_lock;
-  struct sv6621_status_s status;
-  struct sv6621_packet_router_s router;
-  struct sv6621_tx_s tx;
-  struct sv6621_command_engine_s command;
-  struct sv6621_scan_s scan;
-  struct sv6621_service_s service;
-  struct sv6621_rx_s rx;
-  struct sv6621_wifi_info_s wifi_info;
-  struct sv6621_scan_channel_s
-      scan_channels[SV6621_REGULATORY_SCAN_CHANNEL_CAPACITY];
-  size_t scan_channel_count;
-  struct work_s event_work;
-  struct work_s scan_work;
-  int scan_result;
-  bool scan_reporting;
-  bool powered;
-  bool transport_open;
-  bool station_open;
-};
+#define SV6621_REGULATORY_SCAN_CHANNEL_CAPACITY 64
 
-#endif /* __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_CORE_H */
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+int sv6621_regulatory_set_domain(
+    FAR struct sv6621_command_engine_s *command,
+    FAR const struct sv6621_regulatory_domain_s *domain);
+int sv6621_regulatory_scan_channels(
+    FAR const struct sv6621_regulatory_domain_s *domain,
+    FAR struct sv6621_scan_channel_s *channels, size_t capacity,
+    FAR size_t *count);
+
+#endif /* __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_REGULATORY_H */
