2c0acb593177817a50b92273b39630f1e70984a4
BeaconCat <2262965540@qq.com>
2026-08-17 22:22:33 +0800

    wifi: sv6621: validate firmware image metadata
    Parse the aligned kees0616 and evaw0616 metadata markers without C structure overlays. Reject images that target unexpected CP memory and expose the bounded TSVN-to-DEVN slot for a later NV preparation step.
    
    Signed-off-by: BeaconCat <2262965540@qq.com>

---
+++ drivers/wireless/seekwave/sv6621/sv6621_firmware.h
@@ -0,0 +1,56 @@
+/****************************************************************************
+ * drivers/wireless/seekwave/sv6621/sv6621_firmware.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_FIRMWARE_H
+#define __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_FIRMWARE_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <stddef.h>
+#include <stdint.h>
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+struct sv6621_firmware_layout_s
+{
+  uint32_t iram_address;
+  uint32_t dram_address;
+  size_t header_offset;
+  size_t table_end_offset;
+  size_t nv_offset;
+  size_t nv_capacity;
+};
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+int sv6621_firmware_parse_iram(FAR const uint8_t *image, size_t length,
+                               FAR struct sv6621_firmware_layout_s *layout);
+
+#endif /* __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_FIRMWARE_H */
