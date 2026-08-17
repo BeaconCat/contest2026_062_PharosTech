80824411e29b631bee906462cf3788e3de889b09
BeaconCat <2262965540@qq.com>
2026-08-18 00:13:32 +0800

    wifi: sv6621: decode SDIO receive descriptors
    Signed-off-by: BeaconCat <2262965540@qq.com>

---
+++ drivers/wireless/seekwave/sv6621/sv6621_data.h
@@ -0,0 +1,69 @@
+/****************************************************************************
+ * drivers/wireless/seekwave/sv6621/sv6621_data.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_DATA_H
+#define __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_DATA_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define SV6621_DATA_RX_DESCRIPTOR_SIZE 20
+#define SV6621_DATA_RX_PREFIX_SIZE     52
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+struct sv6621_data_rx_s
+{
+  FAR const uint8_t *frame;
+  size_t frame_length;
+  uint16_t sequence;
+  uint8_t fragment;
+  uint8_t instance;
+  uint8_t peer_index;
+  uint8_t tid;
+  bool instance_valid;
+  bool peer_valid;
+  bool multicast;
+  bool eapol;
+};
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+int sv6621_data_decode_rx(FAR const uint8_t *payload, size_t length,
+                          FAR struct sv6621_data_rx_s *rx);
+
+#endif /* __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_DATA_H */
