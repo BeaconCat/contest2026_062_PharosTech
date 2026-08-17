63cda0928c56ef85558394e4cc031fdc44ccaff6
BeaconCat <2262965540@qq.com>
2026-08-18 01:30:29 +0800

    wifi: sv6621: echo authenticator EAPOL versions
    Signed-off-by: BeaconCat <2262965540@qq.com>

--- drivers/wireless/seekwave/sv6621/sv6621_wpa_eapol.h
+++ drivers/wireless/seekwave/sv6621/sv6621_wpa_eapol.h
@@ -64,6 +64,7 @@ struct sv6621_wpa_eapol_s
   enum sv6621_wpa_message_e message;
   FAR const uint8_t *eapol;
   size_t eapol_length;
+  uint8_t version;
   uint16_t key_info;
   uint16_t key_length;
   FAR const uint8_t *replay;
@@ -82,7 +83,7 @@ struct sv6621_wpa_eapol_s
 int sv6621_wpa_eapol_parse(FAR const uint8_t *frame, size_t frame_length,
                             FAR struct sv6621_wpa_eapol_s *eapol);
 int sv6621_wpa_eapol_build(
-    enum sv6621_wpa_response_e response,
+    enum sv6621_wpa_response_e response, uint8_t version,
     FAR const uint8_t replay[SV6621_WPA_REPLAY_SIZE],
     FAR const uint8_t snonce[SV6621_WPA_NONCE_SIZE],
     FAR const uint8_t kck[SV6621_WPA_MIC_SIZE], FAR uint8_t *output,
