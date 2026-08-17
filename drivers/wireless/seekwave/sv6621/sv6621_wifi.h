db445866607c95c29da2c2a936152e318ed95fba
BeaconCat <2262965540@qq.com>
2026-08-17 23:18:21 +0800

    wifi: sv6621: encode firmware MIB values
    Signed-off-by: BeaconCat <2262965540@qq.com>

--- drivers/wireless/seekwave/sv6621/sv6621_wifi.h
+++ drivers/wireless/seekwave/sv6621/sv6621_wifi.h
@@ -66,5 +66,7 @@ int sv6621_wifi_download_calibration(
 int sv6621_wifi_open_station(FAR struct sv6621_command_engine_s *command,
                              FAR const uint8_t address[SV6621_MAC_LENGTH]);
 int sv6621_wifi_close_station(FAR struct sv6621_command_engine_s *command);
+int sv6621_wifi_set_mib(FAR struct sv6621_command_engine_s *command,
+                        uint16_t type, FAR const void *value, uint16_t length);
 
 #endif /* __DRIVERS_WIRELESS_SEEKWAVE_SV6621_SV6621_WIFI_H */
