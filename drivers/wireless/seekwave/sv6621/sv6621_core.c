cb86b865c3a58416ab66529b82b4a3daac5e34ac
BeaconCat <2262965540@qq.com>
2026-08-18 08:43:51 +0800

    wifi: sv6621: advertise firmware HT capabilities
    Signed-off-by: BeaconCat <2262965540@qq.com>

--- drivers/wireless/seekwave/sv6621/sv6621_core.c
+++ drivers/wireless/seekwave/sv6621/sv6621_core.c
@@ -1761,6 +1761,16 @@ int sv6621_start(FAR struct sv6621_dev_s *dev)
       goto fail;
     }
 
+  ret = sv6621_station_configure_ht(
+      &dev->station, dev->wifi_info.ht_capabilities,
+      dev->wifi_info.ht_extended_capabilities,
+      dev->wifi_info.ht_ampdu_parameters, dev->wifi_info.ht_tx_mcs,
+      dev->wifi_info.ht_rx_mcs);
+  if (ret < 0)
+    {
+      goto fail;
+    }
+
   sv6621_data_set_pn_reuse(
       &dev->data, (dev->wifi_info.private_capabilities &
                    SV6621_WIFI_PRIVATE_PN_REUSE) != 0);
