3dfcf33cfbbac1ef6f9218ddd6e6ebf0b6e2b2ff
BeaconCat <2262965540@qq.com>
2026-08-19 16:36:09 +0800

    wifi: sv6621: kick receive after command transmission
    Signed-off-by: BeaconCat <2262965540@qq.com>

--- drivers/wireless/seekwave/sv6621/sv6621_core.c
+++ drivers/wireless/seekwave/sv6621/sv6621_core.c
@@ -69,6 +69,7 @@ static void sv6621_core_service_event(enum sv6621_service_event_e event,
                                       FAR const uint8_t *payload,
                                       size_t length, FAR void *arg);
 static void sv6621_core_rx_error(int error, FAR void *arg);
+static void sv6621_core_command_receive_kick(FAR void *arg);
 static void sv6621_core_command_error(int error, FAR void *arg);
 static void sv6621_core_command_event(uint8_t instance, uint8_t id,
                                       FAR const uint8_t *payload,
@@ -119,6 +120,17 @@ static void sv6621_core_report(FAR struct sv6621_dev_s *dev,
     {
       dev->config.event(dev, event, data, length, dev->config.event_arg);
     }
+}
+
+/****************************************************************************
+ * Name: sv6621_core_command_receive_kick
+ ****************************************************************************/
+
+static void sv6621_core_command_receive_kick(FAR void *arg)
+{
+  FAR struct sv6621_dev_s *dev = arg;
+
+  sv6621_rx_kick(&dev->rx);
 }
 
 /****************************************************************************
@@ -1533,7 +1545,9 @@ int sv6621_create(FAR const struct sv6621_config_s *config,
     }
 
   ret = sv6621_command_engine_init(&dev->command, sv6621_tx_command_sender,
-                                   &dev->tx, sv6621_core_command_event, dev,
+                                   &dev->tx,
+                                   sv6621_core_command_receive_kick, dev,
+                                   sv6621_core_command_event, dev,
                                    sv6621_core_command_error, dev);
   if (ret < 0)
     {
