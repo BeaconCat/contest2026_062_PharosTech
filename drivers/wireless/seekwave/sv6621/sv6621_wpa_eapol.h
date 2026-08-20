182dd6f09d37efbc9e86befd88bfe05482593c8b
BeaconCat <2262965540@qq.com>
2026-08-21 01:15:52 +0800

    wifi: sv6621: build authenticator EAPOL key messages
    Signed-off-by: BeaconCat <2262965540@qq.com>

--- drivers/wireless/seekwave/sv6621/sv6621_wpa_eapol.h
+++ drivers/wireless/seekwave/sv6621/sv6621_wpa_eapol.h
@@ -104,6 +104,15 @@ int sv6621_wpa_eapol_build(
     FAR const uint8_t snonce[SV6621_WPA_NONCE_SIZE],
     FAR const uint8_t kck[SV6621_WPA_MIC_SIZE], FAR uint8_t *output,
     size_t capacity, FAR size_t *written);
+int sv6621_wpa_eapol_build_authenticator(
+    enum sv6621_wpa_message_e message,
+    enum sv6621_wpa_key_mgmt_e key_mgmt, uint8_t version,
+    FAR const uint8_t replay[SV6621_WPA_REPLAY_SIZE],
+    FAR const uint8_t anonce[SV6621_WPA_NONCE_SIZE],
+    FAR const uint8_t kck[SV6621_WPA_MIC_SIZE],
+    FAR const uint8_t kek[SV6621_WPA_KEK_SIZE],
+    FAR const uint8_t *key_data, size_t key_data_length,
+    FAR uint8_t *output, size_t capacity, FAR size_t *written);
 int sv6621_wpa_eapol_verify_mic(
     FAR const struct sv6621_wpa_eapol_s *eapol,
     enum sv6621_wpa_key_mgmt_e key_mgmt,
