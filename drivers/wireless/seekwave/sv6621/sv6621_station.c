341c14aff2e099a14b528707f6250fa74fec6455
BeaconCat <2262965540@qq.com>
2026-08-20 15:01:16 +0800

    wifi: sv6621: encode roaming joins
    Signed-off-by: BeaconCat <2262965540@qq.com>

--- drivers/wireless/seekwave/sv6621/sv6621_station.c
+++ drivers/wireless/seekwave/sv6621/sv6621_station.c
@@ -700,7 +700,7 @@ int sv6621_station_connect(FAR struct sv6621_station_s *station,
 
   ret = sv6621_connection_join(
       station->command, &station->target, station->bandwidth_capabilities,
-      &station->peer);
+      false, &station->peer);
   if (ret < 0)
     {
       sv6621_station_finish(station, SV6621_STATION_IDLE, ret);
