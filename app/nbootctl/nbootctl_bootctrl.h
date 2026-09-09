d04e6dde768084486714de9e663728901f104235
BeaconCat <2262965540@qq.com>
2026-09-10 00:37:56 +0800

    feat(nbootctl): stage an inactive slot
    Signed-off-by: BeaconCat <2262965540@qq.com>

--- app/nbootctl/nbootctl_bootctrl.h
+++ app/nbootctl/nbootctl_bootctrl.h
@@ -30,5 +30,7 @@ int nbootctl_bootctrl_set_active(unsigned int medium, const char *domain,
                                  unsigned int slot);
 int nbootctl_bootctrl_mark_successful(unsigned int medium, const char *domain,
                                       unsigned int slot);
+int nbootctl_bootctrl_stage(unsigned int medium, const char *domain,
+                            unsigned int running_slot, const char *path);
 
 #endif
